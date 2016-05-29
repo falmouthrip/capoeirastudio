9d359f32275dfc1aec9b62c9843f70d4aab8a5f8
munair <munair@qe.local>
2016-05-30 00:06:48 +0900

    add links to image resources on S3 at AWS

--- templates/featured.php
+++ templates/featured.php
@@ -11,7 +11,7 @@
 		<div class="row">
 			<div class="col-lg-12 text-center">
 				<a href="http://home.ebs.co.kr/ebsnews/menu3/newsGlobalView/10344369/H" target="_new">
-				<img src="img/simple-ebs-logo-45x33px-embossed.png" class="wow bounceIn text-primary" data-wow-delay=".1s" style="visibility: visible; animation-name: bounceIn; -webkit-animation-name: bounceIn;">
+				<img src="https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/simple-ebs-logo-45x33px-embossed.png" class="wow bounceIn text-primary" data-wow-delay=".1s" style="visibility: visible; animation-name: bounceIn; -webkit-animation-name: bounceIn;">
 				</a>
 			</div>
 		</div>
--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
 <footer>
 	<div class="container text-center">
 		<div class="col-md-3">
-			<p><a href="http://www.facebook.com/capoeirastudio"><img class="wow bounceIn" data-wow-delay=".1s" src="img/capoeirastudio-alphalogo-90x150px.png"></img></a></p>
+			<p><a href="http://www.facebook.com/capoeirastudio"><img class="wow bounceIn" data-wow-delay=".1s" src="https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/capoeirastudio-alphalogo-90x150px.png"></img></a></p>
 			<p>킬롬볼라 레크리에이션</p>
 			<p>서울특별시중구북창동93-3삼옥빌딩303호</p>
 			<p>휴대폰 번호 : 010-9090-9183</p>
