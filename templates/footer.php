ee67880aff7bf0e5dc1d4c3b0c8271c45968d10d
munair <munair@qe.local>
2016-05-30 14:22:03 +0900

    fix footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer>
-	<div class="container text-center">
-		<div class="col-md-3">
+	<div class="container">
+		<div class="row text-center">
 			<p><a href="http://www.facebook.com/capoeirastudio"><img class="wow bounceIn" data-wow-delay=".1s" src="https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/capoeirastudio-alphalogo-90x150px.png"></img></a></p>
 			<p>킬롬볼라 레크리에이션</p>
 			<p>서울특별시중구북창동93-3삼옥빌딩303호</p>
@@ -9,7 +9,7 @@
 			<p>사업자 등록 번호 : 101-11-14837</p>
 			<p>대표자: Munair Simpson</p>
 		</div>
-		<div class="col-md-6">
+		<div class="row">
     			<?php dynamic_sidebar('sidebar-footer'); ?>
 		</div>
 	</div>
