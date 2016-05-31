021a2d8eda52b5193d5994a7862a30b5100eb644
munair <munair@qe.local>
2016-05-31 15:37:22 +0900

    add logo to footer

--- templates/footer.php
+++ templates/footer.php
@@ -45,6 +45,7 @@
                 </div>
 		<div class="row">
                         <div class="col-xs-12 text-center">
+                                <img src="https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/capoeirastudio-white-icon-644x644px.png" class="wow bounceIn text-primary" data-wow-delay=".1s" style="visibility: visible; animation-name: bounceIn; -webkit-animation-name: bounceIn;">
                                 <h5 class="section-heading">As featured in...</h5>
                                 <hr class="primary">
                                 <a href="http://home.ebs.co.kr/ebsnews/menu3/newsGlobalView/10344369/H" target="_new">
