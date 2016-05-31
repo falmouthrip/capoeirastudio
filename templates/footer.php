4f88f5903e1e8d0624d824b0100e7c1a01a66423
munair <munair@qe.local>
2016-06-01 00:17:46 +0900

    testing new footer copy

--- templates/footer.php
+++ templates/footer.php
@@ -27,12 +27,12 @@
 				?>
 	                </div>
 	                <div class="col-md-3">
-	                        <h2 class="wow bounceIn" data-wow-delay=".1s">COMING SOON</h2>
-	                        <p><a href="">Cantigas</a></p>
-	                        <p><a href="">Ladainhas</a></p>
-	                        <p><a href="">Corridos</a></p>
-	                        <p><a href="">Quadras</a></p>
-	                        <p><a href="">Maculele</a></p>
+	                        <h2 class="wow bounceIn" data-wow-delay=".1s">META</h2>
+	                        <p><a href=""><?php wp_register(); ?></a></p>
+	                        <p><a href=""><?php wp_loginout(); ?></a></p>
+	                        <p><a href=""><?php bloginfo('rss2_url'); ?></a></p>
+	                        <p><a href=""><?php bloginfo('comments_rss2_url'); ?></a></p>
+	                        <p><a href=""><?php esc_attr_e( 'http://wordpress.org/' ); ?></a></p>
 	                </div>
 	                <div class="col-md-3">
 	                        <h2 class="wow bounceIn" data-wow-delay=".1s">REGISTRATION</h2>
