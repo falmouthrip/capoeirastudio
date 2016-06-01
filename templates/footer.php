79c2a35f182007b0e743f27e9e5adeaa8e51c4bb
munair <munair@qe.local>
2016-06-01 11:31:28 +0900

    adding newsletter simple signup link

--- templates/footer.php
+++ templates/footer.php
@@ -31,26 +31,7 @@
 	                        <p><a href="/wp-admin">Site Admin</a></p>
 	                        <p><a href="/logout">Log Out</a></p>
 	                        <h2 class="wow bounceIn" data-wow-delay=".1s">NEWSLETTER</h2>
-	                        <p><!-- Begin MailChimp Signup Form -->
-<div id="mc_embed_signup">
-<form action="//capoeirastudio.us13.list-manage.com/subscribe/post?u=3db435ce999565383d510d2cd&amp;id=64f83024b3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address </label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3db435ce999565383d510d2cd_64f83024b3" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-
-<!--End mc_embed_signup--></p>
+	                        <p><a href="http://eepurl.com/b3Y1Hz">Sign Up</a></p>
 	                </div>
 	                <div class="col-md-3">
 	                        <h2 class="wow bounceIn" data-wow-delay=".1s">REGISTRATION</h2>
