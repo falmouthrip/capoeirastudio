1c4709222121d88f86d214ff8ff2400181a296cb
munair <munair@qe.local>
2016-06-02 11:49:32 +0900

    shift CDO logo left

--- assets/scripts/vegas.js
+++ assets/scripts/vegas.js
@@ -16,7 +16,7 @@ $(".header").vegas({
         { src: "https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/omizumba-header.jpg", valign: "70%", transition: "zoomOut" },
         { src: "https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/teachers-header.jpg", valign: "50%", transition: "swirlRight" },
         { src: "https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/mestresuassuna-black1.jpg", valign: "30%", transition: "zoomOut" },
-        { src: "https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/cdozumbilogo-gold-black-1600x900px-for-iMovie.png", transition: "blur" },
+        { src: "https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/cdozumbilogo-gold-black-1600x900px-for-iMovie.png", align: "10%", transition: "blur" },
       ],
       animation: 'kenburnsUpLeft',
       transitionDuration: 9000,
