b3acccba47e6aeba60eae1836624d0781db568f2
munair <munair@qe.local>
2016-06-04 16:23:44 +0900

    update header icons and remove koreans

--- templates/header.php
+++ templates/header.php
@@ -4,11 +4,11 @@
 			<h1>Rest In Peace<br> Falmouth, Jamaica.</h1>
 			<hr>
 			<p>Committed to making the Falmouth Cemetery an ever beautiful resting place!</p>
-			<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Find Out More</a>
+			<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">Find Out More</a>
 			<hr>
 			<h3 class="white-icons">
 				<div class="col-xs-4">
-					<a href="#contact"><i class="fa fa-envelop"></i></a> CONTACT US
+					<a href="#contact"><i class="fa fa-envelope-o"></i></a> CONTACT US
 				</div>
 				<div class="col-xs-4">
 					<a href="/raffle-ticket/"><i class="fa fa-question-circle-o"></i></a> RAFFLE INFO
