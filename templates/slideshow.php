f96e7a3cf0a2a9fc9a0c0f634d9aaa04e7124ad0
munair <munair@qe.local>
2016-06-04 17:50:14 +0900

    update slideshow to allow folks to volunteer

--- templates/slideshow.php
+++ templates/slideshow.php
@@ -4,7 +4,9 @@
 			<div class="col-lg-12 text-center center-vertically">
 				<h2 class="section-heading">Slideshow</h2>
 				<hr class="primary">
-				<p class="text-muted">These slides are the latest photos from the Falmouth, Cemetery.</p>
+				<p class="text-muted">These slides are the latest photos from the Falmouth, Cemetery. With your support, we are going to make this the most fantastic cemetery in Jamaica.</p>
+				<hr class="primary">
+				<h2 class="section-heading">Volunteer</h2>
 				<p class="text-muted">Get active and take photos of the cemetery.</p>
 				<p class="text-muted"><a href="#contact">Contact</a> Falmouth RIP now to get an account setup to upload photos to this slideshow.</p>
 			</div>
