aa68606155d04d47d517dc33e9871dfb5151db6c
munair <munair@qe.local>
2016-06-04 16:26:31 +0900

    implement logo in introduction and remove korean

--- templates/introduction.php
+++ templates/introduction.php
@@ -2,12 +2,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 text-center">
-				<h2 class="section-heading">Falmouth RIP</h2>
+				<h2 class="section-heading">FALMOUTH</h2>
+				<h2 class="section-heading"><img src="https://s3.amazonaws.com/www-falmouth-rip/rip-icon-inverted-1600x1600px.png" width="150px"></h2>
 				<hr class="light">
 				<p class="text-faded">Falmouth RIP is a website committed to making the Falmouth Cemetery an ever beautiful and sacred place to be burried.</p>
 				<p class="text-faded">The purpose of this website is to connect with others that have a vision of making the Falmouth Cemetery Jamaica's most beautiful resting place. This website is a work-in-progress. However, upon completion you will be able to raise funds for the upkeep of the cemetery grounds and network with others who have (or will have) loved ones burried at the Falmouth Cemetery in Trelawny, Jamaica.</p>
 				<hr class="light">
-				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Learn More</a>
+				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">Learn More</a>
 			</div>
 		</div>
 	</div>
