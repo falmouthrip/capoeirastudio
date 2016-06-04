c9906b88633b7b8e694d95037e0ef01bb2621cd3
munair <munair@qe.local>
2016-06-05 02:16:19 +0900

    reorder text

--- templates/slideshow.php
+++ templates/slideshow.php
@@ -4,10 +4,10 @@
 			<div class="col-lg-12 text-center center-vertically">
 				<h2 class="section-heading">Slideshow</h2>
 				<hr class="primary">
-				<p class="text-muted">These slides are the latest photos from the Falmouth, Cemetery. With your support, we are going to make this the most fantastic cemetery in Jamaica.</p>
 				<p>Falmouth Cemetery has been neglected.  The cemetery is overgrown.  Falmouth is in a period of such economic difficulty, that people are forced build shacks in the cemetery using the graves as flooring.  Relatives are unable to find the graves of their loved ones.</p>
 				<p>On the odd occasions when families raise funds to clear the cemetery, wild goats eat the flowers laid on the graves of those who have passed.</p>
 				<p>Falmouth Cemetary contains the remains of generations of Jamaicans, from the times of slavery, through the struggle for independence, to date.  It is a national treasure, and we owe it to our ancestors to ensure they have a sacred and beautiful resting place.</p>
+				<p class="text-muted">These slides are the latest photos from the Falmouth, Cemetery. With your support, we are going to make this the most fantastic cemetery in Jamaica.</p>
 			</div>
 		</div>
 	</div>
