0bc03ea3cb79b54cae10737a8a4995057fde0f16
munair <munair@qe.local>
2016-06-04 20:30:09 +0900

    add kymani to the header

--- assets/scripts/vegas.js
+++ assets/scripts/vegas.js
@@ -13,10 +13,10 @@ $(".header").vegas({
       preload: true,
       overlay: 'https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/07.png',
       slides: [
-        { src: "https://s3.amazonaws.com/www-falmouth-rip/usain-bolt-1920x1080px.jpg", valign: "70%", transition: "zoomOut" },
+        { src: "https://s3.amazonaws.com/www-falmouth-rip/usain-bolt-1920x1080px.jpg", valign: "25%", transition: "zoomOut" },
         { src: "https://s3.amazonaws.com/www-falmouth-rip/rex-nettleford-1600x1200px.png", valign: "75%", transition: "swirlRight" },
-        { src: "https://s3.amazonaws.com/www-falmouth-rip/ben-johnson-2000x1300px.png", valign: "30%", transition: "zoomOut" },
-        { src: "https://s3.amazonaws.com/www-falmouth-rip/rex-nettleford-1600x1200px.png", align: "10%", transition: "blur" },
+        { src: "https://s3.amazonaws.com/www-falmouth-rip/ben-johnson-2000x1300px.png", valign: "25%", transition: "zoomOut" },
+        { src: "https://s3.amazonaws.com/www-falmouth-rip/kymani-marley-800x1200px.jpg.png", align: "75%", transition: "blur" },
       ],
       animation: 'kenburnsUpLeft',
       transitionDuration: 9000,
