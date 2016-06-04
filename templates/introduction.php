3c29bad526303f3b75cca7a22f31997f50808d6f
munair <munair@qe.local>
2016-06-04 16:32:16 +0900

    implement smaller logo in introduction and update copy

--- templates/introduction.php
+++ templates/introduction.php
@@ -3,10 +3,10 @@
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 text-center">
 				<h2 class="section-heading">FALMOUTH</h2>
-				<h2 class="section-heading"><img src="https://s3.amazonaws.com/www-falmouth-rip/rip-icon-inverted-1600x1600px.png" width="150px"></h2>
+				<h2 class="section-heading"><img src="https://s3.amazonaws.com/www-falmouth-rip/rip-icon-inverted-1600x1600px.png" width="75px"></h2>
 				<hr class="light">
 				<p class="text-faded">Falmouth RIP is a website committed to making the Falmouth Cemetery an ever beautiful and sacred place to be burried.</p>
-				<p class="text-faded">The purpose of this website is to connect with others that have a vision of making the Falmouth Cemetery Jamaica's most beautiful resting place. This website is a work-in-progress. However, upon completion you will be able to raise funds for the upkeep of the cemetery grounds and network with others who have (or will have) loved ones burried at the Falmouth Cemetery in Trelawny, Jamaica.</p>
+				<p class="text-faded">The purpose of this website is to connect with others that have a vision of making the Falmouth Cemetery Jamaica's most beautiful resting place. This website is a work-in-progress. However, upon completion we will to raise funds for the upkeep of the cemetery grounds and have better ability to network with others in Jamaica, and around the world, who have (or will have) loved ones burried at the Falmouth Cemetery in Trelawny, Jamaica.</p>
 				<hr class="light">
 				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">Learn More</a>
 			</div>
