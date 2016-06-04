ecf682985fbebbc546071c66e3f1c42c1c389457
munair <munair@qe.local>
2016-06-05 02:19:08 +0900

    remove korean

--- templates/raffle-info.php
+++ templates/raffle-info.php
@@ -5,8 +5,8 @@
 			<p class="text-muted">"Even if you don't win a raffle prize, you still win because you help the Falmouth Cemetery."</p>
 			<p class="text-right">Franz, <a href="http://www.compucay.com">CompuCay</a> Owner.</p>
 			<hr>
-			<h2>시간표 다운로드 <br>Download Flier</h2>
-			<a href="https://s3.amazonaws.com/www-falmouth-rip/appliance-raffle-ticket.pdf" class="btn btn-default btn-xl wow tada">다운로드 <br>Download</a>
+			<h2>Download Flier</h2>
+			<a href="https://s3.amazonaws.com/www-falmouth-rip/appliance-raffle-ticket.pdf" class="btn btn-default btn-xl wow tada">Download</a>
 		</div>
 	</div>
 </aside>
