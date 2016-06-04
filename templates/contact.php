27732c64c804e8ac79d05a2eb6163434c98503ef
munair <munair@qe.local>
2016-06-04 22:46:30 +0900

    fix footer

--- templates/contact.php
+++ templates/contact.php
@@ -1,5 +1,5 @@
 <section id="contact" class="bg-dark">
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 text-center">
 				<h2 class="section-heading">Let's Get In Touch!</h2>
@@ -19,9 +19,9 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-8 col-lg-offset-2 text-center">
+			<div class="text-center">
 				<hr class="primary">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15135.457043884528!2d-77.6706958763919!3d18.489806698829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eda33f8c3cfab6b%3A0x947175f8cf7d2536!2sFalmouth%2C+Jamaica!5e0!3m2!1sen!2s!4v1465029888587" width="80%" height="40%" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15135.457043884528!2d-77.6706958763919!3d18.489806698829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eda33f8c3cfab6b%3A0x947175f8cf7d2536!2sFalmouth%2C+Jamaica!5e0!3m2!1sen!2s!4v1465029888587" width="100%" height="40%" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
