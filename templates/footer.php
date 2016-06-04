1eed612205bb94f03350d1fdaa5597ec0882f2c4
munair <munair@qe.local>
2016-06-04 17:21:20 +0900

    update footer

--- templates/footer.php
+++ templates/footer.php
@@ -37,12 +37,13 @@
 	                <div class="col-md-3">
 	                        <h2 class="wow bounceIn" data-wow-delay=".1s">FALMOUTH RIP</h2>
 				<p>The purpose of this website is to connect with others that have a vision of making the Falmouth Cemetery Jamaica's most beautiful resting place. This website is a work-in-progress.</p>
+				<p>This website was designed and is maintained by <a href="http://www.quilombola.com/">Quilombola Engineering</a></p>
 	                </div>
                 </div>
 		<div class="row">
                         <div class="col-md-4 col-md-offset-4 text-center">
                                 <img src="https://s3.amazonaws.com/www-falmouth-rip/rip-icon-inverted-1600x1600px.png" class="wow bounceIn text-primary" data-wow-delay=".1s" style="visibility: visible; animation-name: bounceIn; -webkit-animation-name: bounceIn;">
-                                </a>
+				<p class="didot-font">falmouth, jamaica</a>
                         </div>
                 </div>
         </div>
