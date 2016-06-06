99e304778648d179773794015d6dc62dbefc9d25
munair <munair@qe.local>
2016-06-06 20:40:50 +0900

    minor edits

--- templates/introduction.php
+++ templates/introduction.php
@@ -6,7 +6,7 @@
 				<h2 class="section-heading"><img src="https://s3.amazonaws.com/www-falmouth-rip/rip-icon-inverted-1600x1600px.png" width="75px"></h2>
 				<hr class="light">
 				<p class="text-faded">Falmouth RIP is a movement committed to making the Falmouth Cemetery, Trelawny, Jamaica, an ever beautiful and sacred place to be buried.</p>
-				<p class="text-faded">This website aims to connect those all over the world who love Falmouth, and want the generations of upstanding Jamaicans laid to rest there, to have a home that Jamaican’s everywhere are proud of.  Our primary aim to raise funds to ensure the on-going upkeep of the cemetery.</p>
+				<p class="text-faded">This website aims to connect those all over the world who love Falmouth, and want the generations of upstanding Jamaicans laid to rest there, to have a home that Jamaican’s everywhere are proud of.  Our primary aim is to raise funds to ensure the on-going upkeep of the cemetery.</p>
 				<hr class="light">
 				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">Learn More</a>
 			</div>
